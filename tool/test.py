50eee701d357ea25aa56e5ed0e31cd6c61e925ab
李宇茜 <736866102@qq.com>
2023-04-25 22:47:20 +0800

    feat: add pdf tool

---
+++ tool/test.py
@@ -0,0 +1,7 @@
+from PDFReaderTool import read_pdf
+
+pdf_path = r'C:\Users\Administrator\Documents\WeChat ' \
+           r'Files\wxid_5rk05lqmbtq222\FileStorage\File\2023-04\2013—Cell—Inactivation of the BH3-only Protein BAD by ' \
+           r'IKK Inhibits TNFα-induced Apoptosis Independently of NF-κB activation——Q1-41.6.pdf'
+
+read_pdf(pdf_path)
