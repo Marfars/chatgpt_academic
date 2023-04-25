220b40b7f8e7d79e9f400bd54615baf000d74703
李宇茜 <736866102@qq.com>
2023-04-25 23:12:35 +0800

    feat: add test file dir

---
+++ tool/File.py
@@ -0,0 +1,7 @@
+import os
+
+
+def get_filepath_with_filename(filename):
+    parent_dir = os.path.dirname(os.path.dirname(__file__))
+
+    return os.path.join(parent_dir, 'originpaper', filename)
--- tool/test.py
+++ tool/test.py
@@ -1,7 +1,5 @@
+from File import get_filepath_with_filename
 from PDFReaderTool import read_pdf
 
-pdf_path = r'C:\Users\Administrator\Documents\WeChat ' \
-           r'Files\wxid_5rk05lqmbtq222\FileStorage\File\2023-04\2013—Cell—Inactivation of the BH3-only Protein BAD by ' \
-           r'IKK Inhibits TNFα-induced Apoptosis Independently of NF-κB activation——Q1-41.6.pdf'
+read_pdf(get_filepath_with_filename('Vancouver citation style.pdf'))
 
-read_pdf(pdf_path)
