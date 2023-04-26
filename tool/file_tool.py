f789f00a3cc56ce18724dd91b2bdb0454c3923ed
xixi <736866102@qq.com>
2023-04-26 16:12:10 +0800

    feat: add parse pdf file test program

--- tool/file_tool.py
+++ tool/file_tool.py
@@ -4,4 +4,4 @@ import os
 def get_filepath_with_filename(filename):
     parent_dir = os.path.dirname(os.path.dirname(__file__))
 
-    return os.path.join(parent_dir, 'originpaper', filename)
+    return os.path.join(parent_dir, 'origin_paper', filename)
--- tool/test.py
+++ tool/test.py
@@ -1,5 +1,5 @@
-from File import get_filepath_with_filename
-from PDFReaderTool import read_pdf
+from file_tool import get_filepath_with_filename
+from pdf_reader_tool import read_pdf
 
 read_pdf(get_filepath_with_filename('Vancouver citation style.pdf'))
 
